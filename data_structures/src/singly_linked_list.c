193ad6ba3ecf2be1a21fac5ffadca98b1d40756c
akhilmd <akhil.m.durga2@gmail.com>
2016-08-15 13:01:46 +0530

    Documented SLL

--- data_structures/src/singly_linked_list.c
+++ data_structures/src/singly_linked_list.c
@@ -6,10 +6,8 @@ Node* create_from_array(TYPE* ar, int n)
 
     int i = 0;
 
-    for (i=0;i<n;++i)
-    {
+    for (i = 0; i < n; ++i)
         insert_end(&first_node, ar[i]);
-    }
 
     return first_node;
 }
@@ -18,6 +16,7 @@ int insert_end(Node** ptr_first, TYPE in_val)
 {
     Node* new_node = (Node*) malloc(sizeof(Node));
 
+    // If memory could not be allocated
     if (new_node == NULL)
     {
         printf("ERROR!\n");
@@ -27,6 +26,7 @@ int insert_end(Node** ptr_first, TYPE in_val)
     new_node->value = in_val;
     new_node->next = NULL;
 
+    // If list is empty
     if ((*ptr_first) == NULL)
     {
         (*ptr_first) = new_node;
@@ -34,14 +34,14 @@ int insert_end(Node** ptr_first, TYPE in_val)
     }
     else
     {
-        Node* i_node = (*ptr_first);
+        Node* curr_node = (*ptr_first);
 
-        for (; i_node != NULL; i_node = i_node->next)
-            if (i_node->next == NULL)
-            {
-                i_node->next = new_node;
-                return 0;
-            }
+        // Traverse till the last Node
+        for (; curr_node->next != NULL; curr_node = curr_node->next)
+            ;
+
+        curr_node->next = new_node;
+        return 0;
     }
 }
 
@@ -49,14 +49,17 @@ int insert_start(Node** ptr_first, TYPE in_val)
 {
     Node* new_node = (Node*) malloc(sizeof(Node));
 
+    // If memory could not be allocated
     if (new_node == NULL)
     {
         printf("ERROR!\n");
         return 1;
     }
 
+    // new Node points to first Node
     new_node->value = in_val;
     new_node->next = (*ptr_first);
+    // new Node becomes new first Node
     (*ptr_first) = new_node;
 
     return 0;
@@ -64,12 +67,11 @@ int insert_start(Node** ptr_first, TYPE in_val)
 
 int insert_at(Node** ptr_first, TYPE in_val, int pos)
 {
-    if (pos == 1)
-        return insert_start(ptr_first, in_val);
-
     Node* new_node = (Node*) malloc(sizeof(Node));
-    Node* i_node = NULL;
+    Node* prev_node = NULL;
+    Node* curr_node = NULL;
 
+    // If memory could not be allocated
     if (new_node == NULL)
     {
         printf("ERROR!\n");
@@ -79,26 +81,38 @@ int insert_at(Node** ptr_first, TYPE in_val, int pos)
     new_node->value = in_val;
     new_node->next = NULL;
 
-    for (i_node = (*ptr_first), --pos; i_node != NULL && pos > 0; i_node =
-            i_node->next, --pos)
-    {
-        if (pos == 1)
-        {
-            new_node->next = i_node->next;
-            i_node->next = new_node;
-            return 0;
-        }
-    }
+    // Traverse till Node at position to insert in
+    for (curr_node = (*ptr_first); curr_node != NULL && pos > 1; prev_node =
+            curr_node, curr_node = curr_node->next, --pos)
+        ;
 
-    printf("Invalid Position!\n");
-    return 1;
+    // pos is greater than number of nodes or less than 1
+    if (curr_node == NULL || pos < 1)
+    {
+        printf("Invalid Position!\n");
+        return 1;
+    }
+    else
+    {
+        new_node->next = curr_node;
+
+        // If pos is 1
+        if (prev_node == NULL)
+            (*ptr_first) = new_node;
+        else
+            prev_node->next = new_node;
+
+        return 0;
+    }
 }
 
 int delete_start(Node** ptr_first)
 {
     Node *to_be_freed = *ptr_first;
 
+    // Second Node is the new first Node
     (*ptr_first) = (*ptr_first)->next;
+    // Free memory occupied by original first Node
     free(to_be_freed);
 
     return 0;
@@ -106,44 +120,56 @@ int delete_start(Node** ptr_first)
 
 int delete_end(Node** ptr_first)
 {
+    // Do nothing if list is empty
     if ((*ptr_first) == NULL)
         return 0;
 
-    if ((*ptr_first)->next == NULL)
-    {
-        return delete_start(ptr_first);
-    }
+    Node* prev_node = NULL;
+    Node* curr_node = NULL;
 
-    Node* i_node = NULL;
+    // Traverse till last Node.
+    for (curr_node = (*ptr_first); curr_node->next != NULL; prev_node =
+            curr_node, curr_node = curr_node->next)
+        ;
 
-    for (i_node = (*ptr_first); i_node != NULL; i_node = i_node->next)
-        if (i_node->next->next == NULL)
-        {
-            free(i_node->next);
-            i_node->next = NULL;
-            return 0;
-        }
+    // Previous node is new last Node
+    prev_node->next = NULL;
+    // Free Last node
+    free(curr_node);
 
-    return 1;
+    return 0;
 }
 
 int delete_at(Node** ptr_first, int pos)
 {
-    if (pos == 1)
-        return delete_start(ptr_first);
+    // Do nothing if list is empty
+    if ((*ptr_first) == NULL)
+        return 0;
 
-    Node* i_node = NULL;
+    Node* prev_node = NULL;
+    Node* curr_node = NULL;
 
-    for (i_node = (*ptr_first), --pos;i_node != NULL && pos > 0; i_node = i_node->next, --pos)
+    // Traverse till desired Node by decrementing pos till it reaches 1
+    for (curr_node = (*ptr_first); curr_node != NULL && pos > 1; prev_node =
+            curr_node, curr_node = curr_node->next, --pos)
+        ;
+
+    // curr_node can only be NULL if pos is bigger than list length
+    if (pos < 1 || curr_node == NULL)
     {
-        if (pos == 1)
-        {
-            Node *to_be_freed = i_node->next;
-            i_node->next = i_node->next->next;
-            free(to_be_freed);
-            return 0;
-        }
+        printf("Invalid Position!\n");
+        return 1;
     }
+    else
+    {
+        // If pos is 1
+        if (prev_node == NULL)
+            (*ptr_first) = curr_node->next;
+        else
+            prev_node->next = curr_node->next;
+        free(curr_node);
+    }
+
     return 1;
 }
 
@@ -159,6 +185,7 @@ Node* reverse_list(Node* prev, Node* curr)
         curr->next = prev;
     }
 
+    // Base condition.
     if (next == NULL)
         return curr;
 
@@ -167,16 +194,14 @@ Node* reverse_list(Node* prev, Node* curr)
 
 Node* concatenate_lists(Node* list1, Node* list2)
 {
-    Node* i_node = NULL;
+    Node* curr_node = NULL;
 
-    for (i_node = list1;i_node != NULL;i_node = i_node->next)
-    {
-        if (i_node->next == NULL)
+    for (curr_node = list1; curr_node != NULL; curr_node = curr_node->next)
+        if (curr_node->next == NULL)
         {
-            i_node->next = list2;
+            curr_node->next = list2;
             return list1;
         }
-    }
 
     return NULL;
 }
